f58154c935a782819edafb601b798067d17b893f
Teajey <21069848+Teajey@users.noreply.github.com>
2026-01-27 10:54:44 +0900

    Use static namespace and comment for doc reference

--- namespace.go
+++ namespace.go
@@ -1,11 +1,29 @@
 package hyprctl
 
+import (
+	"encoding/xml"
+	"fmt"
+	"runtime/debug"
+)
+
 type Namespace struct {
-	HcXmlns string `xml:"xmlns:c,attr" json:"-"`
+	HcXmlns string      `xml:"xmlns:c,attr" json:"-"`
+	Docs    xml.Comment `xml:",comment" json:"-"`
+}
+
+var docs xml.Comment
+
+func init() {
+	version := "main"
+	if info, ok := debug.ReadBuildInfo(); ok {
+		version = info.Main.Version
+	}
+	docs = xml.Comment(fmt.Sprintf("See documentation for this version of hyprctl at https://github.com/Teajey/hyprctl/blob/%s/README.md", version))
 }
 
 func SetNamespace() Namespace {
 	return Namespace{
-		HcXmlns: "https://github.com/Teajey/hyprctl/blob/v0.4.0/README.md",
+		HcXmlns: "https://github.com/Teajey/hyprctl",
+		Docs:    docs,
 	}
 }
