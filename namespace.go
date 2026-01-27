998687a1fe246fdcdbd94c770fa11811133c6204
Teajey <21069848+Teajey@users.noreply.github.com>
2026-01-27 12:44:36 +0900

    Namespace doc comment

--- namespace.go
+++ namespace.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 )
 
+// Namespace should be embedded in the top-level element to provide context about what the hyper control elements are.
+//
+// SetNamespace should be used to populate the values to their default.
 type Namespace struct {
 	HcXmlns string      `xml:"xmlns:c,attr" json:"-"`
 	Docs    xml.Comment `xml:",comment" json:"-"`
@@ -18,6 +21,7 @@ func init() {
 	docs = xml.Comment(fmt.Sprintf("See an overview of what this XML means at https://%s/blob/main/README.md ", repo))
 }
 
+// SetNamespace provides a default setting for the Namespace struct.
 func SetNamespace() Namespace {
 	return Namespace{
 		HcXmlns: "https://github.com/Teajey/hyprctl",
