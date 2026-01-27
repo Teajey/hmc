298b7f28d72df0e74f7eacdfdbc35e518a748fee
Teajey <21069848+Teajey@users.noreply.github.com>
2026-01-27 12:39:22 +0900

    Abandon provision of version doc reference

--- namespace.go
+++ namespace.go
@@ -3,7 +3,6 @@ package hyprctl
 import (
 	"encoding/xml"
 	"fmt"
-	"runtime/debug"
 )
 
 type Namespace struct {
@@ -15,35 +14,8 @@ var docs xml.Comment
 
 const repo string = "github.com/Teajey/hyprctl"
 
-func getVersion() string {
-	version := "main"
-	info, ok := debug.ReadBuildInfo()
-	if !ok {
-		return version
-	}
-
-	// 1. Check if we are running tests/builds INSIDE the hyprctl repo
-	if info.Main.Path == repo {
-		if info.Main.Version != "" && info.Main.Version != "(devel)" {
-			return info.Main.Version
-		}
-	}
-
-	// 2. Check if we are a dependency of another project
-	for _, dep := range info.Deps {
-		if dep.Path == repo {
-			if dep.Version != "" && dep.Version != "(devel)" {
-				return dep.Version
-			}
-		}
-	}
-
-	return version
-}
-
 func init() {
-	version := getVersion()
-	docs = xml.Comment(fmt.Sprintf("See documentation for this version of hyprctl at https://%s/blob/%s/README.md ", repo, version))
+	docs = xml.Comment(fmt.Sprintf("See an overview of what this XML means at https://%s/blob/main/README.md ", repo))
 }
 
 func SetNamespace() Namespace {
